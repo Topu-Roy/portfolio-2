f885cc0181eb91b39f937e56ce9ea794812d2e0a
Topu-Roy <topuroy143333@gmail.com>
2023-09-21 20:42:54 +0600

    added footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,21 +5,21 @@ type Props = {}
 
 const Footer = (props: Props) => {
     return (
-        <footer className="bg-white dark:bg-gray-900">
+        <footer className="bg-white dark:bg-[#0A0A0A] border-t-2">
             <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
                 <div className="md:flex md:justify-between">
                     <div className="mb-6 md:mb-0">
-                        <a href="https://flowbite.com/" className="flex items-center">
-                            <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="FlowBite Logo" />
-                            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                        <a href="/" className="flex flex-col justify-start items-start">
+                            <span className="self-start text-2xl  whitespace-nowrap dark:text-white font-bold">TOPU ROY</span>
+                            <span className="self-start text-sm whitespace-nowrap dark:text-white">Full Stack Web Developer</span>
                         </a>
                     </div>
                     <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
                         <div>
-                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
+                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Tech Used</h2>
                             <ul className="text-gray-500 dark:text-gray-400 font-medium">
                                 <li className="mb-4">
-                                    <a href="https://flowbite.com/" className="hover:underline">Flowbite</a>
+                                    <a href="https://nextjs.org/" className="hover:underline">NextJS</a>
                                 </li>
                                 <li>
                                     <a href="https://tailwindcss.com/" className="hover:underline">Tailwind CSS</a>
@@ -30,10 +30,10 @@ const Footer = (props: Props) => {
                             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
                             <ul className="text-gray-500 dark:text-gray-400 font-medium">
                                 <li className="mb-4">
-                                    <a href="https://github.com/themesberg/flowbite" className="hover:underline ">Github</a>
+                                    <a href="https://www.facebook.com/topuroy.143/" className="hover:underline ">Facebook</a>
                                 </li>
                                 <li>
-                                    <a href="https://discord.gg/4eeurUVvTy" className="hover:underline">Discord</a>
+                                    <a href="https://www.linkedin.com/in/topu-roy/" className="hover:underline">Linkedin</a>
                                 </li>
                             </ul>
                         </div>
@@ -52,7 +52,7 @@ const Footer = (props: Props) => {
                 </div>
                 <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
                 <div className="sm:flex sm:items-center sm:justify-between">
-                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
+                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Topu Roy™</a>. All Rights Reserved.
                     </span>
                     <div className="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
                         <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
