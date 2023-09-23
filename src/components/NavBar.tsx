6df4ba78c4bc5ff51cb3c4f78087c1fec6a4b0c6
Topu-Roy <topuroy143333@gmail.com>
2023-09-23 21:53:13 +0600

    updated navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,7 +7,7 @@ type Props = {};
 
 function NavBar({ }: Props) {
   return (
-    <header className="w-full mx-auto py-4 border-b-[2px]" id="to_top">
+    <header className="w-full mx-auto py-4 border-b-[2px] sticky top-0 backdrop-blur-3xl z-50" id="to_top">
       <nav className="max-w-7xl mx-auto flex justify-between items-center">
         <li className="flex justify-center items-center gap-4">
           <Link className="font-black text-2xl uppercase" href={'/'}>TOPU</Link>
