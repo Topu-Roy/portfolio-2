db1c64e2f37c9f537d09b55e6ebf925fc6dc0523
Topu-Roy <topuroy143333@gmail.com>
2023-09-23 22:00:55 +0600

    FIX: Fixed border color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,7 +13,7 @@ const Footer = (props: Props) => {
 
                     {/* Profile */}
                     <div className="w-[40%]">
-                        <div className="flex flex-col justify-center items-center border p-2 rounded-md max-w-md mx-auto py-4 px-8">
+                        <div className="flex flex-col justify-center items-center border-[1.5px] border-black/20 p-2 rounded-md max-w-md mx-auto py-4 px-6">
                             {/* Image */}
                             <div className="flex justify-center items-center relative">
                                 <Image src={'/Topu Roy.jpg'} alt='Topu Roy' width={200} height={200} className='rounded-full' />
@@ -34,7 +34,7 @@ const Footer = (props: Props) => {
                             {/* Preview Profile */}
                             <div className="w-full pt-8">
                                 <a href="https://www.fiverr.com/topu_web_dev" target="_blank" rel="noopener noreferrer">
-                                    <button type="button" className='w-full py-2 border border-black dark:border-white text-center rounded-md'>
+                                    <button type="button" className='w-full py-2 border-[1.5px] border-black/20 dark:border-white text-center rounded-md'>
                                         Preview Profile
                                     </button>
                                 </a>
@@ -43,31 +43,32 @@ const Footer = (props: Props) => {
                     </div>
 
                     {/* Gigs */}
-                    <div className="flex-1 rounded-md flex justify-center items-center border p-4 gap-4">
+                    <div className="flex-1 rounded-md flex justify-center items-center border-[1.5px] border-black/20 p-4 gap-4">
 
-                        <div className="flex-1 h-full rounded-md border p-2">
-                            <Image src={'/demo/13.jpg'} alt='' height={200} width={200} className='w-full rounded-md' />
-                            <div className="flex justify-between items-center flex-col text-white/70">
+                        <div className="flex-1 h-full rounded-md border-[1.5px] border-black/20 p-2">
+                            <Image src={'/demo/13.jpg'} alt='' height={200} width={200} className='w-full rounded-md shadow' />
+                            <div className="flex justify-between items-center flex-col text-black/70 dark:text-white/70">
                                 <p className='font-bold py-2'>I will build professional web applications using Next and Tailwind CSS</p>
 
-                                <div className="flex w-full justify-between items-center">
+                                <div className="flex w-full justify-between items-center font-semibold">
                                     5 Star (50)
                                     <p>Starting at <span className="text-2xl font-bold">$20</span></p>
                                 </div>
                             </div>
                         </div>
 
-                        <div className="flex-1 h-full rounded-md border p-2">
-                            <Image src={'/demo/14.jpg'} alt='' height={600} width={600} className='w-full rounded-md' />
-                            <div className="flex justify-between items-center flex-col text-white/70">
+                        <div className="flex-1 h-full rounded-md border-[1.5px] border-black/20 p-2">
+                            <Image src={'/demo/14.jpg'} alt='' height={200} width={200} className='w-full rounded-md shadow' />
+                            <div className="flex justify-between items-center flex-col text-black/70 dark:text-white/70">
                                 <p className='font-bold py-2'>I will build professional web applications using Next and Tailwind CSS</p>
 
-                                <div className="flex w-full justify-between items-center">
+                                <div className="flex w-full justify-between items-center font-semibold">
                                     5 Star (50)
                                     <p>Starting at <span className="text-2xl font-bold">$20</span></p>
                                 </div>
                             </div>
                         </div>
+
                     </div>
                 </div>
             </div>
