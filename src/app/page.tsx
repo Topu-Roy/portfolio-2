9bd368aacdc3252d55290a8a42a3770562b0bf88
Topu-Roy <topuroy143333@gmail.com>
2023-09-24 15:35:17 +0600

    UX: Made responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className="flex justify-center items-center mx-auto max-w-7xl flex-col min-h-[90vh]">
       {/* Hero Section */}
-      <section className='pb-16'>
+      <section className='pb-16 px-3 md:px-0'>
         {/* Headline */}
         <h1 className="text-black dark:text-white text-6xl font-bold text-center pt-16 pb-4">Full Stack NextJS Developer</h1>
         <div className="w-full mx-auto text-center text-xl font-medium pb-4">
