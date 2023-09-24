38df4382b66769ab5bfd3fede5524c1460d50844
Topu-Roy <topuroy143333@gmail.com>
2023-09-24 15:06:05 +0600

    UX: Nav responsive

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -12,7 +12,7 @@ function NavBar() {
 
   return (
     <header
-      className="sticky top-0 z-50 mx-auto w-full border-b-[2px] px-3 py-4 backdrop-blur-3xl sm:px-0"
+      className="sticky top-0 z-50 mx-auto w-full border-b-[2px] px-3 py-4 backdrop-blur-3xl xl:px-0"
       id="to_top"
     >
       <nav className="relative mx-auto flex max-w-7xl items-center justify-between">
@@ -35,37 +35,46 @@ function NavBar() {
           </Button>
         </div>
 
+
+        {/* Mobile slider */}
         <div
           className={`${!isOpen ? "translate-x-[150%]" : ""
-            } absolute left-[-0.75rem] top-[-1rem] z-50 mx-auto flex h-[100vh] w-[100vw] flex-col justify-start bg-white pt-16 dark:bg-black`}
+            } absolute transition-transform duration-200 left-[-0.75rem] top-[-1rem] z-[100] mx-auto flex h-[100vh] w-[100vw] flex-col justify-start bg-white px-3 py-4 dark:bg-black`}
         >
-          <div className="relative flex flex-col items-center justify-center gap-8 ">
+          <div className="w-full flex justify-end items-center">
             <Button
-              className="absolute right-[10%] top-[-20%] z-[999] bg-white font-semibold  text-black dark:bg-black dark:text-white"
+              className="bg-white font-semibold  text-black dark:bg-black dark:text-white"
               onClick={() => setIsOpen(false)}
               variant="outline"
             >
               Close
             </Button>
+          </div>
+          <div className="flex flex-col items-center justify-center gap-8 ">
 
             <h3 className="text-4xl font-bold">Go To</h3>
             <Button
               onClick={() => setIsOpen(false)}
               variant={"ghost"}
-              className="m-0 p-0"
               type="submit"
             >
               <Link href={"/"}>Home </Link>
             </Button>
 
-            <Link
+            <Button
               onClick={() => setIsOpen(false)}
-              className="flex items-center justify-center"
-              href={"/template"}
+              variant={"ghost"}
+              type="submit"
             >
-              Template
-              <ArrowSVG />
-            </Link>
+              <Link
+                onClick={() => setIsOpen(false)}
+                className="flex items-center justify-center"
+                href={"/template"}
+              >
+                Template
+                <ArrowSVG />
+              </Link>
+            </Button>
 
             <HireNowButton onClick={() => setIsOpen(true)} />
           </div>
